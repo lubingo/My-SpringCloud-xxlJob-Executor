017c1b35a1fac04bfd41591ac0fa8b7e8e164f56
lubing <15810172080@139.com>
2019-10-08 17:21:34 +0800

    优化 -剔除数据库连接

--- src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
+++ src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
@@ -2,10 +2,9 @@ package com.xxl.job.executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class XxlJobExecutorApplication {
 
     public static void main(String[] args) {
